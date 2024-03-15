2fba77d79a5a97eb8579336d3e46dee17a34948a
Edu2413771 <2413771@cesba-queretaro.edu.mx>
2024-03-15 09:58:37 -0600

    1er Commit

---
+++ src/mx/cesba/edu/HolaMundo.java
@@ -0,0 +1,35 @@
+package mx.cesba.edu;
+
+
+public class HolaMundo {
+    
+    public static void main(String[] args){
+        /*
+        int edad = 17;
+        int mayorEdad;
+        
+        mayorEdad= edad+1;
+        
+        //Esta es una linea de impresion en consola
+        System.out.println("Bienvenidos al mundo de la programacion");
+        System.out.println("El joven es menor de edad: "+edad);
+        System.out.println("Ya es un adulto: "+mayorEdad);
+        
+        float precio = 78.93F;
+        float iva = 10.01F;
+        float precioTotal;
+        
+        precioTotal = precio + iva;
+        
+        System.out.println("Precio total: "+ precioTotal);
+        
+        char corchete = 93;
+        System.out.println(corchete);
+        
+        
+        //Tipo de dato LONG
+        
+        long suma1 = 987787767;
+        long suma2 = 877766666;*/
+    }
+}
